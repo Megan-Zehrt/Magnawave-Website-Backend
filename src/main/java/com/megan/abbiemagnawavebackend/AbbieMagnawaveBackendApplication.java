ff988dc8bab0d4c34364081a71098da29ca204e3
Megan Zehrt <zehrtmegan@gmail.com>
2025-07-12 14:29:39 -0500

    Initial Commit

---
+++ src/main/java/com/megan/abbiemagnawavebackend/AbbieMagnawaveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.megan.abbiemagnawavebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbbieMagnawaveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbbieMagnawaveBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/megan/abbiemagnawavebackend/AbbieMagnawaveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megan.abbiemagnawavebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbbieMagnawaveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
